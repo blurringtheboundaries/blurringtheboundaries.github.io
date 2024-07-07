2e947cd1433289090fe5ba5362ff3839c67971d5
Charles Matthews <charles0matthews@gmail.com>
2024-07-07 16:17:35 -0400

    Update sensor-music.js

--- colour-testing/instrument/sensor-music.js
+++ colour-testing/instrument/sensor-music.js
@@ -2,15 +2,21 @@
  * A quick musical sketch based on our current microbit lantern setup, trying out the built-in speaker on the microbit v2.
  */
 
+// base octave: C3 = 130.81 Hz
+const frequences = [130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94];
+
 
 input.onButtonPressed(Button.AB, function () {
     music.stopAllSounds()
     music.play(music.tonePlayable(784, music.beat(BeatFraction.Breve)), music.PlaybackMode.LoopingInBackground)
 })
-let pin1 = 0
-let pin0 = 0
-serial.redirectToUSB()
-music.setBuiltInSpeakerEnabled(true)
+
+let pin1 = 0;
+let pin0 = 0;
+
+serial.redirectToUSB();
+music.setBuiltInSpeakerEnabled(true);
+
 loops.everyInterval(20, function () {
     if (input.buttonIsPressed(Button.A)) {
         pin0 = Math.map(Math.constrain(pins.analogReadPin(AnalogPin.P0), 100, 150), 150, 100, 0, 127)
@@ -35,6 +41,7 @@ loops.everyInterval(20, function () {
         music.stopAllSounds()
     }
 })
+
 basic.forever(function () {
 	
 })
