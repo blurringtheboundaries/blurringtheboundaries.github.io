5e18b8e7e78b58a204c15866055cfc75058619c9
Charles Matthews <charles0matthews@gmail.com>
2024-07-08 20:32:55 -0400

    experimenting with sensor music demo

--- colour-testing/instrument/sensor-music.js
+++ colour-testing/instrument/sensor-music.js
@@ -3,45 +3,52 @@
  */
 
 // base octave: C3 = 130.81 Hz
-const frequences = [130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94];
+const frequencies = [130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94];
 
+const pinsArray = [AnalogPin.P0, AnalogPin.P1];
+let pinValues = [0, 0];
+
+function readAndMapPins() {
+    for (let i = 0; i < pinsArray.length; i++) {
+        pinValues[i] = Math.map(
+            Math.constrain(pins.analogReadPin(pinsArray[i]), 100, 150),
+            150,
+            100,
+            0,
+            127
+        );
+    }
+}
+
+function playToneIfNeeded(pinIndex, tone) {
+    if (pinValues[pinIndex] > 10) {
+        music.stopAllSounds();
+        music.play(music.tonePlayable(tone, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground);
+        music.setVolume(pinValues[pinIndex]);
+        if (pinValues[0] > 10 && pinValues[1] > 10) {
+            basic.pause(Math.map(pinValues[pinIndex], 0, 127, 20, 40));
+        }
+    }
+}
 
 input.onButtonPressed(Button.AB, function () {
-    music.stopAllSounds()
-    music.play(music.tonePlayable(784, music.beat(BeatFraction.Breve)), music.PlaybackMode.LoopingInBackground)
-})
-
-let pin1 = 0;
-let pin0 = 0;
+    music.stopAllSounds();
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Breve)), music.PlaybackMode.LoopingInBackground);
+});
 
 serial.redirectToUSB();
 music.setBuiltInSpeakerEnabled(true);
 
 loops.everyInterval(20, function () {
     if (input.buttonIsPressed(Button.A)) {
-        pin0 = Math.map(Math.constrain(pins.analogReadPin(AnalogPin.P0), 100, 150), 150, 100, 0, 127)
-        pin1 = Math.map(Math.constrain(pins.analogReadPin(AnalogPin.P1), 100, 150), 150, 100, 0, 127)
-        if (pin0 > 10) {
-            music.stopAllSounds()
-            music.play(music.tonePlayable(659, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground)
-            music.setVolume(pin0)
-            if (pin0 > 10 && pin1 > 10) {
-                basic.pause(Math.map(pin0, 0, 127, 20, 40))
-            }
-        }
-        if (pin1 > 10) {
-            music.stopAllSounds()
-            music.play(music.tonePlayable(523, music.beat(BeatFraction.Breve)), music.PlaybackMode.InBackground)
-            music.setVolume(pin1)
-            if (pin0 > 10 && pin1 > 10) {
-                basic.pause(Math.map(pin1, 0, 127, 20, 40))
-            }
-        }
+        readAndMapPins();
+        playToneIfNeeded(0, frequencies[0]);
+        playToneIfNeeded(1, frequencies[5]);
     } else {
-        music.stopAllSounds()
+        music.stopAllSounds();
     }
-})
+});
 
 basic.forever(function () {
-	
-})
+    // Forever loop if needed
+});
